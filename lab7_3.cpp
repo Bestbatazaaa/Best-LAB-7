7368150218d1ce38181458b3c72286ea7617f45a
Bestbatazaaa <Bestbatazaaa@icloud.com>
2023-12-08 16:31:18 +0700

    lab7_3 completed

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
 using namespace std;
 
-char before(char x){
-	//Write your function definition here
+	char before(char x) {
+     	if(x < 65 or x > 90){
+     		return '0';
+     	}
+     	if(x == 'A'){
+    		 return 'Z';
+     	}else{
+     		return x-1;
+     	}
 }
 
 int main(){
